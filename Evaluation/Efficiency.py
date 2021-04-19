f5aa5fea7e896b9063f884df31af92ba451df74d
Jun <1259389904@qq.com>
2021-04-19 14:49:41 +0800

    Add files via upload

---
+++ Evaluation/Efficiency.py
@@ -0,0 +1,55 @@
+import os, time
+import json,glob
+import argparse
+from pynvml.smi import nvidia_smi
+from multiprocessing import Process
+
+
+def daemon_process(time_interval, json_path, gpu_index=0):
+    gpu_memory_max = 0
+    while True:
+        nvsmi = nvidia_smi.getInstance()
+        dictm = nvsmi.DeviceQuery('memory.free, memory.total')
+        gpu_memory = dictm['gpu'][gpu_index]['fb_memory_usage']['total'] - dictm['gpu'][gpu_index]['fb_memory_usage'][
+            'free']
+        if os.path.exists(json_path):
+            with open(json_path)as f:
+                js = json.load(f)
+        else:
+            js = {
+            'gpu_memory':[]
+            }
+        with open(json_path, 'w')as f:
+            #js['gpu_memory'] = gpu_memory_max
+            js['gpu_memory'].append(gpu_memory)
+            json.dump(js, f, indent=4)
+        time.sleep(time_interval)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-time_interval", default=0.5, help="time_interval")
+    parser.add_argument("-shell_path", default='predict.sh', help="time_interval")
+    parser.add_argument("-gpus", default=0, help="CUDA_VISIBLE_DEVICES")
+    parser.add_argument("-docker_input_file", default='./inputs/', help="docker input folder") 
+    parser.add_argument("-docker_name", default='nnunet', help="docker output folder") 
+    args = parser.parse_args()
+    print('We are evaluating', args.docker_name)
+    json_dir = './data_all/{}'.format(args.docker_name)
+    json_path = os.path.join(json_dir,glob.glob(args.docker_input_file+'/*')[0].split('/')[-1].split('.')[0]+'.json')
+    print(json_path)
+    p1 = Process(target=daemon_process, args=(args.time_interval, json_path, args.gpus,))
+    p1.daemon = True
+    p1.start()
+    t0 = time.time()
+    cmd = 'docker container run --gpus=1  --name {} --rm -v $PWD/inputs/:/workspace/inputs/ -v $PWD/outputs/:/workspace/outputs/ {}:latest /bin/bash -c "sh {}"'.format(
+    args.docker_name, args.docker_name, args.shell_path)
+    print(cmd) 
+    os.system(cmd)
+
+    infer_time = time.time() - t0
+    with open(json_path, 'r')as f:
+        js = json.load(f)
+    with open(json_path, 'w')as f:
+        js['time'] = infer_time
+        json.dump(js, f, indent=4)
---
+++ Evaluation/run.py
@@ -0,0 +1,19 @@
+import shutil,os
+import glob
+import torch
+
+tarlist = glob.glob('./team_docker/*.tar')
+for item in tarlist:
+    name = item.split('/')[-1].split('.')[0].lower()
+    #print(name )
+    os.system('docker image load < {}'.format(item))
+    os.mkdir('./data_all/{}'.format(name))
+    for image_i in  glob.glob('imagesTs/*'):
+        shutil.copy(image_i,image_i.replace('imagesTs','inputs'))
+        os .system('python Efficiency.py -docker_name {}'.format(name))
+        os.remove(image_i.replace('imagesTs','inputs'))
+    os.system('python load_json.py -docker_name {}'.format(name))
+    shutil.move('./outputs','./data_all/{}/'.format(name))
+    os.mkdir('./outputs')
+    torch.cuda.empty_cache()
+    ## break
