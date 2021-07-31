e292c9dac65c47e2ba836f2833de19f1cd0694f0
anxingle <axingle@infervision.com>
2021-07-31 22:56:57 +0800

    [CHORE] add more :memo:

--- Evaluation/Efficiency.py
+++ Evaluation/Efficiency.py
@@ -75,7 +75,7 @@ if __name__ == "__main__":
         p1.start()
         t0 = time.time()
         # XXX: in case of someone use lower docker, please use specified GPU !!!
-        # cmd = 'nvidia docker run --runtime="nvidia" --name {} --rm -v $PWD/inputs/:/workspace/input/ -v $PWD/inputs/:/workspace/inputs/ -v $PWD/outputs/:/workspace/outputs/ {}:latest /bin/sh -c "sh {}"'.format(
+        # cmd = 'docker container run --runtime="nvidia"  -e NVIDIA_VISIBLE_DEVICES=1 --name {} --rm -v $PWD/inputs/:/workspace/input/ -v $PWD/inputs/:/workspace/inputs/ -v $PWD/outputs/:/workspace/outputs/ {}:latest /bin/sh -c "sh {}"'.format(
         #     args.docker_name, args.docker_name, args.shell_path
         # )
         cmd = 'docker container run --gpus="device=1" --name {} --rm -v $PWD/inputs/:/workspace/inputs/ -v $PWD/outputs/:/workspace/outputs/ {}:latest /bin/bash -c "sh predict.sh" '.format(
